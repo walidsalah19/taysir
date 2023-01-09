152ab5c2391287ac04ac872702eeafb237890a91
walid salah <walidsalah9585@gmail.com>
2023-01-09 23:40:54 +0200

    update

---
+++ app/src/main/java/com/example/taysir/UserAccess/CustomerProfile.java
@@ -0,0 +1,29 @@
+package com.example.taysir.UserAccess;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taysir.R;
+
+public class CustomerProfile extends Fragment {
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer_profile, container, false);
+    }
+}
