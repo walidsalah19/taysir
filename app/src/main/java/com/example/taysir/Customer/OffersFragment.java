947e95b31371f79677f26d5a66493db63ca36964
walid salah <walidsalah9585@gmail.com>
2023-01-10 18:30:25 +0200

    update

---
+++ app/src/main/java/com/example/taysir/Customer/OffersFragment.java
@@ -0,0 +1,27 @@
+package com.example.taysir.Customer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taysir.R;
+
+public class OffersFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_offers, container, false);
+    }
+}
